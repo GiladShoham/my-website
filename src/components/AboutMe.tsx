eb8e7fc426b4a95144544ffe6e3d383cfc707627
Gilad Shoham <shoham.gilad@gmail.com>
2024-10-22 20:48:20 +0300

    remove unused import

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,8 +9,7 @@ import {
   PenTool,
   MessageSquare,
   ChevronLeft,
-  ChevronRight,
-  Github,
+  ChevronRight
 } from 'lucide-react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
